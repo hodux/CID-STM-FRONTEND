7971f894767a29d12608a0cb005a58dac5f57d34
Yves-Shaheem Shedid <2233669@collegemv.qc.ca>
2024-12-04 16:56:54 -0500

    merge dev into signin

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import Header from "./components/Header";
 import AppRoutes from "./routes/Routes";
 
 function App() {
   return (
     <Router>
       <div>
-        <Header />
         <AppRoutes />
       </div>
     </Router>
