c26eaaba54c79b4f92786816279aee47eef634d7
Yves-Shaheem Shedid <2233669@collegemv.qc.ca>
2024-12-04 15:42:41 -0500

    init react app tailwind css

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="">
+      <header className="">
+        Bienvenue dans le Travail Pratique Finale du cours Collecte et interpretation.
+      </header>
+      <div>
+      <button>Signin</button>
+      <button>Signup</button>
+      </div>
+    </div>
+  );
+}
+export default App;
