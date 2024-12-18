e0fc6a747d8f206709dbed2b2e19b5970e0cc8ff
Jimmy <jimmy.n4444@gmail.com>
2024-12-18 13:55:58 -0500

    add filter logic to the trip time chart

--- src/pages/FullTripTimeChart.js
+++ src/pages/FullTripTimeChart.js
@@ -7,6 +7,8 @@ const FullTripTimeChart = () => {
     const baseUrl = "https://localhost:3001/api/trips/duration";
     const [data, setData] = useState([]);
     const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [busFilter,setBusFilter]=useState("");
+    const [durationMinimumFilter,setDurationMinimumFilter]=useState(0);
     useEffect( () => {
         const fetchData = async () => {
             try{
@@ -27,12 +29,36 @@ const FullTripTimeChart = () => {
         }
         fetchData()
     }, [navigate, isAuthenticated])
-    data.map((trip) => {
-        console.log(trip)
-    })
+    const filteredData = (!busFilter && durationMinimumFilter===0)? data: 
+    data.filter((trip) => {
+        const matchesBus = !busFilter || trip.routeId==busFilter; 
+        const matchesDuration = durationMinimumFilter ===0 || trip.duration >= durationMinimumFilter;
+        return matchesBus && matchesDuration; 
+    });    
+
+
     return (
         <div className=" h-screen w-screen self-center items-center p-5 flex">
-            <BarChartTrip data={data} />
+            <div className="mb-5">
+                <input
+                    type="text"
+                    placeholder="Filtrer par autobus..."
+                    value={busFilter}
+                    onChange={e => setBusFilter(e.target.value)}
+                    className="border p-2 rounded"
+                />
+            </div>
+
+            <div className="mb-5">
+                <input
+                    type="number"
+                    placeholder="Filtrer par temps..."
+                    value={durationMinimumFilter}
+                    onChange={e => setDurationMinimumFilter(e.target.value)}
+                    className="border p-2 rounded"
+                />
+            </div>
+            <BarChartTrip data={filteredData} />
         </div>
 
     )
